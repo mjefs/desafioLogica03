58ae0e44421025c13606ccadcd5fe5041f446ad7
Jeferson Nascimento <mjef013@gmail.com>
2025-05-04 19:59:19 -0300

    Meu projeto

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        /*3 - Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e B forem iguais, deverá somar os dois valores, caso contrário devera
+        multiplicar A por B. Ao final de qualquer um dos cálculos deve-se atribuir o resultado a uma variável C eimprimir seu valor na tela.*/
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a == b){
+            int c = a + b;
+            System.out.println("A e B são iguais então o valor somado é: " + c);
         }
+        else {
+            int c = a * b;
+            System.out.println("A e B são dieferentes então o valor multiplicado é: " + c);
+        }
+
+        sc.close();
     }
 }
